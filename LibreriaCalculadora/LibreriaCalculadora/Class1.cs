515b12d70e2b53011ef0a39546d6550050fbca98
Kippyru <kfornier@gmail.com>
2025-10-03 17:02:07 -0300

    Libreria para Winforms con operaciones de calculo

---
+++ LibreriaCalculadora/LibreriaCalculadora/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaCalculadora
+{
+    public class libCalculadora
+    {
+        private double _resultado;
+        private string _operacion;
+
+        public double Resultado => _resultado;
+
+        public void SetNumero(double numero)
+        {
+            _resultado = numero;
+        }
+
+        public void SetOperacion(string operacion)
+        {
+            _operacion = operacion;
+        }
+
+        public double Calcular(double segundoNumero)
+        {
+            switch (_operacion)
+            {
+                case "+":
+                    _resultado = _resultado + segundoNumero;
+                    break;
+                case "-":
+                    _resultado = _resultado - segundoNumero;
+                    break;
+                case "*":
+                    _resultado = _resultado * segundoNumero;
+                    break;
+                case "/":
+                    _resultado = segundoNumero == 0
+                        ? (_resultado >= 0 ? double.PositiveInfinity : double.NegativeInfinity)
+                        : _resultado / segundoNumero;
+                    break;
+                default:
+                    break;
+            }
+            return _resultado;
+        }
+
+        public void Limpiar()
+        {
+            _resultado = 0;
+            _operacion = string.Empty;
+        }
+    }
+}
+
