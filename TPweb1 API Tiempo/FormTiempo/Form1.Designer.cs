796f6e1d96453bf7687b542082b7e399208ac273
Kippyru <kfornier@gmail.com>
2025-11-15 17:46:35 -0300

    API tiempo

---
+++ TPweb1 API Tiempo/FormTiempo/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace FormTiempo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dateTxt = new System.Windows.Forms.TextBox();
+            this.tempTxt = new System.Windows.Forms.TextBox();
+            this.summaryTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.actualizarBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "TIEMPO";
+            // 
+            // dateTxt
+            // 
+            this.dateTxt.Location = new System.Drawing.Point(173, 82);
+            this.dateTxt.Name = "dateTxt";
+            this.dateTxt.Size = new System.Drawing.Size(100, 20);
+            this.dateTxt.TabIndex = 1;
+            this.dateTxt.TextChanged += new System.EventHandler(this.dateTxt_TextChanged);
+            // 
+            // tempTxt
+            // 
+            this.tempTxt.Location = new System.Drawing.Point(173, 138);
+            this.tempTxt.Name = "tempTxt";
+            this.tempTxt.Size = new System.Drawing.Size(100, 20);
+            this.tempTxt.TabIndex = 2;
+            this.tempTxt.TextChanged += new System.EventHandler(this.tempTxt_TextChanged);
+            // 
+            // summaryTxt
+            // 
+            this.summaryTxt.Location = new System.Drawing.Point(173, 205);
+            this.summaryTxt.Name = "summaryTxt";
+            this.summaryTxt.Size = new System.Drawing.Size(100, 20);
+            this.summaryTxt.TabIndex = 3;
+            this.summaryTxt.TextChanged += new System.EventHandler(this.summaryTxt_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(42, 89);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Date";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(42, 145);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(92, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Temperature in C°";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(42, 212);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Summary";
+            // 
+            // actualizarBtn
+            // 
+            this.actualizarBtn.Location = new System.Drawing.Point(173, 16);
+            this.actualizarBtn.Name = "actualizarBtn";
+            this.actualizarBtn.Size = new System.Drawing.Size(100, 23);
+            this.actualizarBtn.TabIndex = 7;
+            this.actualizarBtn.Text = "Actualizar Datos";
+            this.actualizarBtn.UseVisualStyleBackColor = true;
+            this.actualizarBtn.Click += new System.EventHandler(this.actualizarBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 273);
+            this.Controls.Add(this.actualizarBtn);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.summaryTxt);
+            this.Controls.Add(this.tempTxt);
+            this.Controls.Add(this.dateTxt);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "TiempoApp";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox dateTxt;
+        private System.Windows.Forms.TextBox tempTxt;
+        private System.Windows.Forms.TextBox summaryTxt;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button actualizarBtn;
+    }
+}
+
---
+++ TPweb1 API Tiempo/FormTiempo/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Text.Json;
+
+namespace FormTiempo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        public class WeatherForecast
+        {
+            public string date { get; set; }
+            public int temperatureC { get; set; }
+            public string summary { get; set; }
+        }
+
+        private void actualizarBtn_Click(object sender, EventArgs e)
+        {
+            actualizarBtn_Click(sender, e, new HttpClient());
+        }
+
+        private async void actualizarBtn_Click(object sender, EventArgs e, HttpClient client)
+        {
+            try
+            {
+                
+                string url = "https://localhost:7015/WeatherForecast";
+
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+
+                var json = await response.Content.ReadAsStringAsync();
+
+                
+                var weatherList = JsonSerializer.Deserialize<List<WeatherForecast>>(json);
+
+                if (weatherList != null && weatherList.Count > 0)
+                {
+                    var firstWeather = weatherList[0];
+
+                    dateTxt.Text = firstWeather.date;
+                    tempTxt.Text = firstWeather.temperatureC.ToString();
+                    summaryTxt.Text = firstWeather.summary;
+                }
+                else
+                {
+                    MessageBox.Show("No se recibieron datos del clima.");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al obtener datos: " + ex.Message);
+            }
+        }
+
+        private void dateTxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tempTxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void summaryTxt_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
